bfb44fa18efe3a9a19341912d14b5176942db2df
huy1412 <huytgse170167@fpt.edu.vn>
2026-06-10 13:26:35 +0700

    done version 1.2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	turbopack: {
+		root: import.meta.dirname,
+	},
+	images: {
+		qualities: [75, 90],
+		remotePatterns: [
+			{ protocol: "https", hostname: "vinhome.com.vn", pathname: "/**" },
+			{ protocol: "https", hostname: "vietnamland.vn", pathname: "/**" },
+			{ protocol: "https", hostname: "alphareal.vn", pathname: "/**" },
+			{ protocol: "https", hostname: "chamdalat.vn", pathname: "/**" },
+			{ protocol: "https", hostname: "www.act.com.vn", pathname: "/**" },
+		],
+	},
 };
 
 export default nextConfig;
